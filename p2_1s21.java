795756bd8e0d6ee995aac6fcda2ac96112c147c0
AlanYaelCarpioCardona <144179270+AlanYaelCarpioCardona@users.noreply.github.com>
2023-09-05 16:09:46 -0600

    Add files via upload
    practica 1 y 2 del dia 5 de septiembre 2023

---
+++ p2_1s21.java
@@ -0,0 +1,11 @@
+class p2_1s21{
+	public static void main(String[] args) {
+		System.out.println("Datos personales");
+     Nombre.prinln("alan yael carpio cardona")
+            Edad.prinln("17")
+       Correo.println ("acarpiocardona@gmail.com") 
+      Aficiones.println ("escuchar musica") 
+       Color.println ("morado")
+      }
+}
+
