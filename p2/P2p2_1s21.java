e19807740f29cb345d5cbe977a969fd072954b66
AlanYaelCarpioCardona <144179270+AlanYaelCarpioCardona@users.noreply.github.com>
2023-10-10 21:10:45 -0600

    Add files via upload

---
+++ p2/P2p2_1s21.java
@@ -0,0 +1,32 @@
+public class P2p2_1s21{
+	public static void main(String[] args) {
+		int val=1;
+		switch(val){
+		case 1: System.out.println("se evalua a 1"); break;
+		case 2: System.out.println("se evalua a 2"); break;
+		case 3: System.out.println("se evalua a 3"); break;
+		case 4: System.out.println("se evalua a 4"); break;
+		case 5: System.out.println("se evalua a 5"); break;
+		default: System.out.println("valor fuera del rango");
+		}
+
+		Character opc='a';
+		switch(opc){
+		case 'a' : System.out.println("se evaluo A");
+		case 'b' : System.out.println("se evaluo B");
+		case 'c': System.out.println("Se evaluo C");
+		case 'd': System.out.println("Se evaluo D");			
+		case 'e': System.out.println("Se evaluo E");
+		case 'f': System.out.println("Se evaluo F");
+		default: System.out.println("opcion no existe");
+	}
+	String cadena = "uno";
+	switch (cadena) {
+	case "uno":System.out.println("uno");
+	case "dos":System.out.println("dos");
+	case "tres":System.out.println("tres");
+	case  "cuatro":System.out.println("cuatro");
+	default. System.out.println("no existe la opcion");
+}
+}
+}
